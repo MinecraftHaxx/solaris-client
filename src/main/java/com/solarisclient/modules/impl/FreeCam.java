643022329e32021b19f609100d7c92025af55383
MinecraftHaxx <wateraudi64@gmail.com>
2025-09-10 22:25:40 +0300

    Create FreeCam.java

---
+++ src/main/java/com/solarisclient/modules/impl/FreeCam.java
@@ -0,0 +1,8 @@
+package com.solarisclient.modules.impl;
+
+import com.solarisclient.modules.Module;
+
+public class FreeCam extends Module {
+    public FreeCam() { super("Free Cam"); }
+    // TODO: Implement FreeCam logic
+}
